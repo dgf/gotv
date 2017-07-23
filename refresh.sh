8acfdade560fddbff27c49a51f884c3b96244a20
dgf <deep@dagnu.de>
2017-07-24 01:32:12 +0200

    refresh goreport

--- refresh.sh
+++ refresh.sh
@@ -5,13 +5,22 @@ set -ev
 main=github.com/dgf/gotv
 
 # godoc.org refresh URL
-godocURL=http://godoc.org/-/refresh
+godocURL=https://godoc.org/-/refresh
 
 # refresh godoc
 godocRefresh() {
 	curl -D - -X POST -d "path=$1" $godocURL
 }
 
+# goreport.org refresh URL
+goreportURL=https://goreportcard.com/checks
+
+# refresh goreport
+goreportRefresh() {
+	curl -D - -X POST -d "repo=$1" $goreportURL
+}
+
+goreportRefresh "$main"
 godocRefresh "$main"
 for package in $(find . -mindepth 1 -type d | grep -v .git | grep -v testdata | cut -d '/'  -f 2); do
 	godocRefresh "$main/$package"
